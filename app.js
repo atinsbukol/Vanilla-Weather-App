646a4d2acf8b5761b1f4331ef1ccb832d0bf0624
Bukola Agunbiade <atinsbukol@gmail.com>
2023-05-26 10:36:36 +0100

    Add Current Location

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ function getforecast(coordinate) {
 }
 
 function displayTemperature(response) {
+  //console.log(response)
 
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -112,7 +113,7 @@ function handlesubmit(event) {
     event.preventDefault()
     let cityInputElement = document.querySelector("#city-input")
     search(cityInputElement.value)
-    console.log(cityInputElement.value)
+    //console.log(cityInputElement.value)
 }
 
 function displayFahrenheitTemperature(event) {
@@ -137,6 +138,23 @@ function displayCelsiusTemperature(event) {
     fahrenheitLink.classList.remove("active");
 
 }
+
+function currentLocation(position) {
+apiKey = "6ce66d083b4d6dddb74ba02266495c46"
+   apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayTemperature)
+  console.log(apiUrl)
+}
+
+
+function getcurrentLocation(event) {
+  event.preventDefault()
+  navigator.geolocation.getCurrentPosition(currentLocation);
+
+}
+
+let currentLocationButton = document.querySelector("#current-location")
+currentLocationButton.addEventListener("click", getcurrentLocation)
 
 let celciusTemperature = null
 
